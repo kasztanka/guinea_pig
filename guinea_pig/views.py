6ade78ce44e336835fc191f07c9278749dbba9e6
pioter <piotr@atopik.pl>
2016-05-14 11:11:26 +0200

    First working view file. Renamed templates.

--- guinea_pig/views.py
+++ guinea_pig/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.template import loader
+
+#def function(request):
+#    data = some_data
+#    template = loader.get_template("path_to_template")
+#    context = {'arg1': data}
+#    return HttpResponse(template.render(context, request))
+
+#def function(request):
+#    data = some_data
+#    context = {'arg1': data}
+#    return render(request, "path_to_template", context)
 
 def index(request):
-    return HttpResponse("We love you guyyyys")
+    header_text = "Welcome to our website!"
+    context = {"header_text": header_text}
+    return render(request, "guinea_pig/index.html", context)
+
