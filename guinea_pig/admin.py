88207c3c79a546bd966c587063fdaf09d71f4d22
piotr <piotr@atopik.pl>
2016-05-30 11:53:32 +0200

    Fixed bug in __str__ method of Comment model. Added modifying comment for admins.

--- guinea_pig/admin.py
+++ guinea_pig/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Game, Record
+from .models import Game, Record, Comment
 
 admin.site.register(Game)
 admin.site.register(Record)
+admin.site.register(Comment)
