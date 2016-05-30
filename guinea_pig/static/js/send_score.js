9bc16a5a472b72d52c2ec1fb72d7cb64c6ce058a
kasztanka <m.lipowska@onet.pl>
2016-05-30 16:49:00 +0200

    Games send score only when user is logged in. Added templatetag to game.name in profile.html

--- guinea_pig/static/js/send_score.js
+++ guinea_pig/static/js/send_score.js
@@ -1,8 +1,10 @@
 function sendScore(score) {
     var result;
+    if (LOGGED_IN) {
     $.get('score', {'score': score}, function(data) {
         result = data;
         getHighscores();
     });
+    }
     return result;
 }
